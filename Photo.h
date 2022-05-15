f4e1e0651cffa9ec8cef80b8e8e1e5a7a240ad58
ravii-sa <IT21296246@my.sliit.lk>
2022-05-15 19:01:10 +0530

    complaints, date, photo, posttype

---
+++ Photo.h
@@ -0,0 +1,26 @@
+#include <cstring>
+class Photo
+{
+	private:
+		int photoID;
+		char location[50];
+		float size;
+
+	public:
+		Photo()
+		{
+			photoID = 0;
+			strcpy(location, "images/default.png");
+			size = 32.0;
+		};
+		Photo(int pPhotoID, const char pLocation[], float pSize)
+		{
+			photoID = pPhotoID;
+			strcpy(location, pLocation);
+			size = pSize;
+		};
+		void displayPhoto();
+		void showDetails();
+		~Photo();
+};
+
