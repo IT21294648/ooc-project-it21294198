54a948d4f37a4d7925ca5bfb4f85eaeebce073ab
IT21292668 <99679572+IT21292668@users.noreply.github.com>
2022-05-17 12:09:20 +0530

    Update RequestPost.h

--- RequestPost.h
+++ RequestPost.h
@@ -18,14 +18,13 @@ public:
             strcpy(address,"default")
            };
 
-	     RequestPost(float rprice,float rlandArea,const char raddress[100], int postId , User *user )
-            : Post( postId, Title[30], location[30], Province[30], description[100], City[30], District[30], photos[5], date, *complaint[2],*postType[1],*user)
+ RequestPost(float rprice,float rlandArea,const char raddress[100],int pid,const char title[30],const char locate[30],const char descript[100],const char city[30]
+ ,const char district[30],const char province[30],Photo rphotos[5],Date rdate,Complaint *rcomplaint[2],PostType *rpostType[1],User *ruser)
+            : Post( pid, title[30], locate[30], descript[100],city[30], district[30],province[30],rphotos[5],rdate,*rcomplaint[2],*rpostType[1],*ruser)
            {
             price=rprice;
             landArea=rlandArea;
             strcpy(address,raddress);
-           
-
            };
       void setSPdetails (float rprice , float rlandarea, string raddress  , int rpostId , User *ruser ,  date  rdate, photo rphotos[10]);
 	void displaySPdetails();
