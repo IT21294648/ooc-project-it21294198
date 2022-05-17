b44d0a4719941d52758ce1bcbd003306474aa2da
IT21292668 <99679572+IT21292668@users.noreply.github.com>
2022-05-17 15:30:40 +0530

    Update RequestPost.h

--- RequestPost.h
+++ RequestPost.h
@@ -20,7 +20,7 @@ public:
 
  RequestPost(float rprice,float rlandArea,const char raddress[100],int pid,const char title[30],const char locate[30],const char descript[100],const char city[30]
  ,const char district[30],const char province[30],Photo rphotos[5],Date rdate,Complaint *rcomplaint[2],PostType *rpostType[1],User *ruser)
-            : Post( pid, title[30], locate[30], descript[100],city[30], district[30],province[30],rphotos[5],rdate,*rcomplaint[2],*rpostType[1],*ruser)
+            : Post( pid, title, locate, descript,city, district,province,rphotos,rdate,rcomplaint,*rpostType,ruser)
            {
             price=rprice;
             landArea=rlandArea;
