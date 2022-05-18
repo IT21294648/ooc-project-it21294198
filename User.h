c7c16ddba393b4acea3f8d745dc82bf42b2d2953
cha_wick <99271238+IT21294648@users.noreply.github.com>
2022-05-18 17:50:50 +0530

    updated user class

---
+++ User.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <cstring>
+#include "Photo.h"
+#include "Payment.h"
+class User{
+private:
+	int id;
+	char name[100];
+	char address[100];
+	char email[100];
+	char password[100];
+	char about[500];
+	char status[50];
+	Photo* profilePhoto;
+
+public:
+	User(){
+		id = 0;
+		strcpy(name,"default");
+		strcpy(address,"default");
+		strcpy(email,"default");
+		strcpy(password,"default");
+		strcpy(about,"default");
+		strcpy(status,"default");
+		profilePhoto = new Photo();
+	}
+	User( int pId, const char pName[], const char pAddress[], const char pEmail[], const char pPassword[], const char pAbout[], const char pStatus[],  Photo* pProfilephoto)
+	{
+		id = pId;
+		strcpy(name,pName);
+		strcpy(address,pAddress);
+		strcpy(email,pEmail);
+		strcpy(password,pPassword);
+		strcpy(about,pAbout);
+		strcpy(status,pStatus);
+		profilePhoto = pProfilephoto;
+	}
+	
+	
+	void setDetails( int pId, const char pName[], const char pAddress[], const char pEmail[], const char pPassword[], const char pAbout[], const char pStatus[],  Photo* pProfilephoto);
+	void makePayment(Payment payments);
+	void getUserDetails();
+	void viewPost(Post post);
+};
