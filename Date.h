b9602aec0a693c9711749154bec9dd8bfe68db07
ravii-sa <IT21296246@my.sliit.lk>
2022-05-18 17:46:55 +0530

    date

---
+++ Date.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <cstring>
+class Date{
+private:
+	int day;
+	int month;
+	int year;
+
+public:
+	Date(){
+		day = 0;
+		month = 0;
+		year = 0;
+	}
+	Date(int pDay, int pMonth, int pYear)
+	{
+		day = pDay;
+		month = pMonth;
+		year = pYear;
+	}
+
+	void setDetails(int pDay, int pMonth, int pYear);
+	char* getDateString();
+	Date* calculateDate(int days);
+	int getDay();
+	int getMonth();
+	int getYear();
+};
