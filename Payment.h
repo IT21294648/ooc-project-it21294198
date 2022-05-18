f6cfa8e07b85899ae662af01c77e64d0409a8a6b
it21294198 <99207728+it21294198@users.noreply.github.com>
2022-05-18 17:49:47 +0530

    payment1

---
+++ Payment.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <cstring>
+#include "Date.h"
+class Payment{
+private:
+	int id;
+	char type;
+	char method[30];
+	float amount;
+	Date* paymentDate;
+
+public:
+	Payment(){
+		id = 0;
+		type = 'A';
+		strcpy(method,"default");
+		amount = 0.0;
+		paymentDate = new Date();
+	}
+	Payment( int pId,  char pType, const char pMethod[],  float pAmount,  Date* pPaymentdate)
+	{
+		id = pId;
+		type = pType;
+		strcpy(method,pMethod);
+		amount = pAmount;
+		paymentDate = pPaymentdate;
+	}
+    void getAmout(float pamount);
+    void enterDetails(char ptype,int pid,float pmethod);
+    float calculateTotal();
+    void calculateEndDate(Date edate);
+    void displayDetails();
+    //~Payment();
+};
