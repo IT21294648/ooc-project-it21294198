32b2053e148b5d2025c58fcc0122de932771f7f1
ravii-sa <IT21296246@my.sliit.lk>
2022-05-18 17:44:40 +0530

    new files

---
+++ Complaint.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <cstring>
+#include "Date.h"
+#include "User.h"
+class Complaint{
+private:
+	int id;
+	char type[50];
+	char description[500];
+	Date* date;
+	bool reviewed;
+	User* owner;
+
+public:
+	Complaint(){
+		id = 0;
+		strcpy(type,"default");
+		strcpy(description,"default");
+		date = new Date();
+		reviewed = false;
+		owner = new User();
+	}
+	Complaint(int pId, char pType[], char pDescription[], Date* pDate, bool pReviewed, User* pOwner)
+	{
+		id = pId;
+		strcpy(type,pType);
+		strcpy(description,pDescription);
+		date = pDate;
+		reviewed = pReviewed;
+		owner = pOwner;
+	}
+
+	void setDetails(int pComplaintID, const char pComplaintType[], const char pDescription[], Date* pDate, bool pReviewed, int pOwnerID, int pPostID);
+	void showDetails();
+	User* getOwner();
+	Post* getComplainedPost();
+};
---
+++ main.cpp
@@ -0,0 +1,29 @@
+
+#include "Post.h"
+#include "SalePost.h"
+#include "RequestPost.h"
+#include "User.h"
+#include "Moderator.h"
+#include "Admin.h"
+#include "Complaint.h"
+#include "PostType.h"
+#include "Date.h"
+#include "Photo.h"
+#include "Payment.h"
+
+int main()
+{
+	Post post;
+	SalePost salepost;
+	RequestPost requestPost;
+	User user;
+	Moderator moderator;
+	Admin admin;
+	Complaint complaint;
+	PostType postType;
+	Date date;
+	Photo photo;
+	Payment payment;
+}
+
+
