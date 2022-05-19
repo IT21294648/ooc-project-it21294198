61e0e7ec9d112550d189dd7e464103b146ea19db
Akmal <99236776+IT21292422@users.noreply.github.com>
2022-05-19 17:15:57 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,20 +13,21 @@
 
 int main()
 {
-	Date date1;
-	Date date2(1, 1, 2021);
-
-	Photo photo1;
-	Photo photo2(1, "images/photo2.png", 32);
-
-	PostType postType1;
-	PostType postType2(1, 2000.0, "Silver", "The post will be valid for 60 days");
-
-	Complaint complaint1;
-	Complaint complaint2(1, "false advertisement", "the land does not match the given details", new Date(), false, new User());
-
+	Post post1;
+	Post post2(1,"Land for sale in Maharagama","7.2906N,80.6337E","Maharagama","Colombo","Western Province","Land available for sale in the heart of Maharagama. Price Negotiable", new PostType(), new Date(), new User());
+	SalePost salepost;
+	RequestPost requestPost;
+	User user;
+	Moderator moderator1;
+	Moderator moderator2(3,"Sunil Perera","27 Kandy Road Katugasthota","sunilp@gmail.com","password@123","Hello World","Available", new Photo());
+	Admin admin;
+	Complaint complaint;
+	PostType postType;
+	Date date;
+	Photo photo;
+	
 	Payment payment1;
-    Payment payment2('B',1001,"Card",500.00,new Date());
+    	Payment payment2('B',1001,"Card",500.00);
 }
 
 
