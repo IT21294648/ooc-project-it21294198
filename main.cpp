637bc19a1b3d72466c500962212d41ce68b33bc3
ravii-sa <IT21296246@my.sliit.lk>
2022-05-19 17:08:25 +0530

    main

--- main.cpp
+++ main.cpp
@@ -13,17 +13,17 @@
 
 int main()
 {
-	Post post;
-	SalePost salepost;
-	RequestPost requestPost;
-	User user;
-	Moderator moderator;
-	Admin admin;
-	Complaint complaint;
-	PostType postType;
-	Date date;
-	Photo photo;
-	Payment payment;
+	Date date1;
+	Date date2(1, 1, 2021);
+
+	Photo photo1;
+	Photo photo2(1, "images/photo2.png", 32);
+
+	PostType postType1;
+	PostType postType2(1, 2000.0, "Silver", "The post will be valid for 60 days");
+
+	Complaint complaint1;
+	Complaint complaint2(1, "false advertisement", "the land does not match the given details", new Date(), false, new User());
 }
 
 
